e03932dc6ba85f301b23a23f940d426a09beb425
idavidjunior <i.davidjunior@gmail.com>
2026-07-25 18:25:04 -0300

    Fix .gitignore to exclude build/

--- app/build/intermediates/cxx/Debug/393e5q55/logs/armeabi-v7a/build_command_fingerprint_jni.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Playtec-bancada\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Playtec-bancada\\Desktop\\Codigos\\Android\\Mp3Player\\app\\.cxx\\Debug\\393e5q55\\armeabi-v7a" ^
-  fingerprint_jni
